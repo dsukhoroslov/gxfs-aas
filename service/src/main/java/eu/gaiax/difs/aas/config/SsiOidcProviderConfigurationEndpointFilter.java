fe32de550c1fa54178b8d0b686b732201336a2ce
Steffen Schulze <steffen.schulze@t-systems.com>
2023-01-04 00:08:36 +0100

    cors.

--- service/src/main/java/eu/gaiax/difs/aas/config/SsiOidcProviderConfigurationEndpointFilter.java
+++ service/src/main/java/eu/gaiax/difs/aas/config/SsiOidcProviderConfigurationEndpointFilter.java
@@ -93,7 +93,7 @@ public final class SsiOidcProviderConfigurationEndpointFilter extends OncePerReq
                 .build();
 
         ServletServerHttpResponse httpResponse = new ServletServerHttpResponse(response);
-        httpResponse.getHeaders().add("Access-Control-Allow-Origin", "https://integration.gxfs.dev");
+        httpResponse.getHeaders().add("Access-Control-Allow-Origin", "*");
         this.providerConfigurationHttpMessageConverter.write(providerConfiguration, MediaType.APPLICATION_JSON, httpResponse);
     }
 
